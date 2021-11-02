eeacb7c8e2eaa6035f4d087781488e8828ef5b2e
lstyxr <526560086@qq.com>
2021-11-02 17:22:24 +0800

    modify init settings

--- hello/settings.py
+++ hello/settings.py
@@ -1,1 +1,9 @@
-# ≈‰÷√Œƒº˛
+# ÈÖçÁΩÆÊñá‰ª∂
+import os
+from hello import app
+
+dev_db = 'sqlite:///' + os.path.join(os.path.dirname(app.root_path), 'data.db')
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
